66ca302be4e99132934054462edecb6558aebb6a
Kevin Yobeth <leokeviny@gmail.com>
2022-11-27 15:22:49 +0700

    Update meta tag

--- src/components/layouts/Chooser.tsx
+++ src/components/layouts/Chooser.tsx
@@ -14,15 +14,23 @@ const PlaygroundChip = (playground: Playground) => (
 export default function ChooserLayout({ children }: PropsWithChildren) {
   const { pathname } = useRouter();
 
+  const page = AVAILABLE_PLAYGROUND.find((playground) => playground.url === pathname);
+
+  const title = pathname === '/' ? 'Playground - Kevin Yobeth' : 'Playground - ' + page?.title;
+  const description = page?.description;
+
   return (
     <>
       <Head>
-        <title>
-          {pathname === '/'
-            ? 'Playground - Kevin Yobeth'
-            : 'Playground - ' + AVAILABLE_PLAYGROUND.find((playground) => playground.url === pathname)?.title}
-        </title>
+        <title>{title}</title>
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        <meta name='description' content={description} />
+
+        <meta property='og:title' content={title} />
+        <meta property='og:description' content={description} />
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://playground.kevinyobeth.com' />
+        <meta property='og:image' content='https://playground.kevinyobeth.com/og-image.png' />
       </Head>
       <div className='relative flex flex-col justify-between w-screen h-screen border-red-700'>
         {children}
